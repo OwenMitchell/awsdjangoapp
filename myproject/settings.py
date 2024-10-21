e2c73570779146b184bf6e5e347d8bf166610c3d
Owen Mitchell <owenrossmitchell@gmail.com>
2024-10-21 18:33:29 -0400

    added signup app with dummy view

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-za4urp!5ubiyayny2we9=i^(fk#^mhxcgg736!i1ldlt*#!vxi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".awsapprunner.com"]
 
 
 # Application definition
