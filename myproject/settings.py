13d8d5c2b8666bb8d9f67ed183dbbe798063e0a7
Owen Ross Mitchell <oven@Owens-Mac-mini.local>
2024-11-02 18:08:10 -0400

    added logging to django settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -186,3 +186,25 @@ CORS_ALLOW_METHODS = [
     "DELETE",
     "OPTIONS",
 ]
+
+import logging
+
+# Django logging settings
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "/var/log/django/debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
